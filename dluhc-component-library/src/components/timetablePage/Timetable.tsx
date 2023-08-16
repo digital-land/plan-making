166dde39918487a167900ec1deb3f9893b0d4558
Andrew Jones <97245023+amjones-scottlogic@users.noreply.github.com>
2023-08-16 13:54:47 +0100

    Add story book (#7)
    * Add story book
    
    * Add map story
    
    * Remove typo

--- dluhc-component-library/src/components/timetablePage/Timetable.tsx
+++ dluhc-component-library/src/components/timetablePage/Timetable.tsx
@@ -56,7 +56,7 @@ const Timetable = ({ data }: StagesProps) => {
                 <th key={header.id} className="py-3">
                   {flexRender(
                     header.column.columnDef.header,
-                    header.getContext()
+                    header.getContext(),
                   )}
                 </th>
               ))}
---
+++ dluhc-component-library/src/stories/TimetablePage.stories.tsx
@@ -0,0 +1,13 @@
+import TimetablePage from "../components/timetablePage/TimetablePage";
+
+export default {
+  component: TimetablePage,
+  title: "Timetable",
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    filepath: "/src/mockData/timetable.json",
+  },
+};
