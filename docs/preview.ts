c9653128f46aaafae5b50ce4a5e9867bcf2c6fbf
amjones-scottlogic <97245023+amjones-scottlogic@users.noreply.github.com>
2023-08-17 09:29:54 +0000

    Deploying to gh-pages from  @ c4fd67fc06b77c00374715a5c5868567607595de 🚀

---
+++ docs/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/preact";
+
+import "../src/index.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
