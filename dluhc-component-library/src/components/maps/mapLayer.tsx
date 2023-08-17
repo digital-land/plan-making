c2a6fb99ff06b1806edcbb86fd8e9536f5fa7152
Sam Ornsby <samornsby@gmail.com>
2023-08-17 12:20:13 +0100

    DLUHC-204 add component for rendering GeoJSON polygon layers

---
+++ dluhc-component-library/src/components/maps/mapLayer.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { useMap } from "../../contexts/mapContext";
+import { FeatureCollection } from "geojson";
+import VectorSource from "ol/source/Vector";
+import GeoJSON from "ol/format/GeoJSON";
+import VectorImageLayer from "ol/layer/VectorImage";
+import Style from "ol/style/Style";
+import Fill, { Options as FillOptions } from "ol/style/Fill";
+import Stroke, { Options as StrokeOptions } from "ol/style/Stroke";
+import { useRef } from "preact/hooks";
+import BaseLayer from "ol/layer/Base";
+
+interface MapLayerProps {
+  features: FeatureCollection;
+  stroke: StrokeOptions;
+  fill: FillOptions;
+}
+
+const MapLayer = ({ features, stroke, fill }: MapLayerProps) => {
+  const map = useMap();
+  const layer = useRef<BaseLayer | null>(null);
+
+  useEffect(() => {
+    if (layer.current) {
+      map.removeLayer(layer.current);
+      layer.current = null;
+    }
+    const source = new VectorSource({
+      features: new GeoJSON().readFeatures(features),
+    });
+    layer.current = new VectorImageLayer({
+      source,
+      style: new Style({
+        fill: new Fill(fill),
+        stroke: new Stroke(stroke),
+      }),
+    });
+    map.addLayer(layer.current);
+    return () => {
+      layer.current && map.removeLayer(layer.current);
+    };
+  }, [features, map, layer, stroke, fill]);
+
+  return <></>;
+};
+
+export default MapLayer;
