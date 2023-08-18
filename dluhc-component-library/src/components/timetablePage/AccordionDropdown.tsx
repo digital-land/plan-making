41fe0f32e0b985850d1c5c3c29c122864c7c25c0
gcooperSL <110818566+gcooperSL@users.noreply.github.com>
2023-08-18 15:28:56 +0100

    added children instead of props

--- dluhc-component-library/src/components/timetablePage/AccordionDropdown.tsx
+++ dluhc-component-library/src/components/timetablePage/AccordionDropdown.tsx
@@ -1,31 +1,33 @@
 import { useState } from "react";
 import { IndicationArrow } from "../inlinesvgs/IndicationArrow";
+import { ComponentChildren } from "preact";
 
-const AccordionDropdown = ({ information }: { information?: string }) => {
-  const [isClicked, setClicked] = useState(false);
+interface AccordionDropdownProps {
+  children?: ComponentChildren;
+}
 
-  const toggle = () => {
-    isClicked == true ? setClicked(false) : setClicked(true);
+const AccordionDropdown = ({ children }: AccordionDropdownProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleDropdownClicked = () => {
+    isExpanded == true ? setIsExpanded(!isExpanded) : setIsExpanded(true);
   };
 
+  const rotationClass = isExpanded ? "rotate-90" : "";
+
   return (
-    <>
-      <div className="pt-1 pb-3">
-        <IndicationArrow
-          className={
-            "h-3 w-3 transition-all inline-block" +
-            (isClicked ? " rotate-90" : "")
-          }
-        />
-        <span
-          className="text-blue-400 underline cursor-pointer pl-2"
-          onClick={() => toggle()}
-        >
-          More information
-        </span>
-        {isClicked ? <div class="border-l-4 p-4 m-1">{information}</div> : null}
-      </div>
-    </>
+    <div className="pt-1 pb-3">
+      <IndicationArrow
+        className={`h-3 w-3 transition-all inline-block ${rotationClass}`}
+      />
+      <span
+        className="text-blue-400 underline cursor-pointer pl-2"
+        onClick={handleDropdownClicked}
+      >
+        More information
+      </span>
+      {isExpanded && <div class="border-l-4 p-4 m-1">{children}</div>}
+    </div>
   );
 };
 
