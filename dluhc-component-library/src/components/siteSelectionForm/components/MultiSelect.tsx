6de69a30e7904fe7d39f35aa1c6e5fe7c4aa3a7d
Andrew Jones <97245023+amjones-scottlogic@users.noreply.github.com>
2023-08-22 11:28:40 +0100

    Add form state (#22)

--- dluhc-component-library/src/components/siteSelectionForm/components/MultiSelect.tsx
+++ dluhc-component-library/src/components/siteSelectionForm/components/MultiSelect.tsx
@@ -1,20 +1,36 @@
 interface MultiSelectProps {
-  values: ReadonlyArray<String>;
+  options: ReadonlyArray<string>;
+  values?: Record<string, boolean>;
+  onChange: (values: Record<string, boolean>) => void;
 }
 
-const MultiSelect = ({ values }: MultiSelectProps) => {
-  if (!values.length) {
+const MultiSelect = ({ options, values = {}, onChange }: MultiSelectProps) => {
+  if (!options.length) {
     return null;
   }
 
-  const options = values.map((value) => (
-    <div key={value} className="flex items-center mb-4">
-      <input type="checkbox" class="checkbox" />
-      <label className="ml-2 font-semibold">{value}</label>
+  const handleChange = (key: string, value: boolean) => {
+    onChange({
+      ...values,
+      [key]: value,
+    });
+  };
+
+  const optionComponents = options.map((key) => (
+    <div key={key} className="flex items-center mb-4">
+      <label className="font-semibold flex">
+        <input
+          type="checkbox"
+          class="checkbox mr-2"
+          checked={values[key]}
+          onClick={() => handleChange(key, !values[key])}
+        />
+        <span>{key}</span>
+      </label>
     </div>
   ));
 
-  return <div>{options}</div>;
+  return <div>{optionComponents}</div>;
 };
 
 export default MultiSelect;
