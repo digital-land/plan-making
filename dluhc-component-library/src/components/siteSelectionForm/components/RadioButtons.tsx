ff3b1962d90304d6ab1776c5ea565475c8e08e8b
Beth Pritchard <bpritchard@scottlogic.com>
2023-08-23 10:11:16 +0100

    wip

---
+++ dluhc-component-library/src/components/siteSelectionForm/components/RadioButtons.tsx
@@ -0,0 +1,36 @@
+interface MultiSelectProps {
+  options: ReadonlyArray<string>;
+  values?: Record<string, boolean>;
+  onChange: (values: Record<string, boolean>) => void;
+}
+
+const RadioButtons = ({ options, values = {}, onChange }: MultiSelectProps) => {
+  if (!options.length) {
+    return null;
+  }
+
+  const handleChange = (key: string, value: boolean) => {
+    onChange({
+      ...values,
+      [key]: value,
+    });
+  };
+
+  const optionComponents = options.map((key) => (
+    <div key={key} className="flex items-center mb-4">
+      <label className="font-semibold flex">
+        <input
+          type="checkbox"
+          class="checkbox mr-2"
+          checked={values[key]}
+          onClick={() => handleChange(key, !values[key])}
+        />
+        <span>{key}</span>
+      </label>
+    </div>
+  ));
+
+  return <div>{optionComponents}</div>;
+};
+
+export default RadioButtons;
