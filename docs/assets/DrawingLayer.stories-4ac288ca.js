cde09c50b7bdb036ab836aff9549c919c5b13d43
lfranchino <93914515+lfranchino@users.noreply.github.com>
2023-08-30 12:54:04 +0000

    Deploying to gh-pages from  @ 3a6d26ce7616538d2e02719c9b82c05e5d39abe9 🚀

---
+++ docs/assets/DrawingLayer.stories-4ac288ca.js
@@ -0,0 +1,22 @@
+import{M as s}from"./MapComponent-218dea8f.js";import{o as i}from"./jsxRuntime.module-8e7d8b33.js";import"./MultiPolygon-adcb1022.js";import"./hooks.module-10a4f742.js";import"./preact.module-4f38c496.js";import"./compat.module-b1a3416f.js";import"./drawingLayer-a4e13dd5.js";const p=({baseMapProps:a,drawingMapProps:n})=>i(s,{baseMapProps:a,drawingMapProps:n}),M={component:p,title:"Components/Map/Drawing Layer",tags:["autodocs"]},r={args:{baseMapProps:{lat:54.97,lng:-1.65,zoom:10,isDrawingMode:!0,style:{height:"500px",width:"500px"}},drawingMapProps:{strokeColor:"#ffcc33",fillColor:"rgba(255, 255, 255, 0.2)",strokeWidth:2,circleRadius:7,circleFillColor:"#ffcc33"}}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    baseMapProps: {
+      lat: 54.97,
+      lng: -1.65,
+      zoom: 10,
+      isDrawingMode: true,
+      style: {
+        height: "500px",
+        width: "500px"
+      }
+    },
+    drawingMapProps: {
+      strokeColor: "#ffcc33",
+      fillColor: "rgba(255, 255, 255, 0.2)",
+      strokeWidth: 2,
+      circleRadius: 7,
+      circleFillColor: "#ffcc33"
+    }
+  }
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const h=["Default"];export{r as Default,h as __namedExportsOrder,M as default};
+//# sourceMappingURL=DrawingLayer.stories-4ac288ca.js.map
---
+++ docs/assets/Map.stories-7b44dc86.js
@@ -0,0 +1,15 @@
+import{M as s}from"./MapComponent-218dea8f.js";import{o as n}from"./jsxRuntime.module-8e7d8b33.js";import"./MultiPolygon-adcb1022.js";import"./hooks.module-10a4f742.js";import"./preact.module-4f38c496.js";import"./compat.module-b1a3416f.js";import"./drawingLayer-a4e13dd5.js";const p=({baseMapProps:r})=>n(s,{baseMapProps:r}),M={component:p,title:"Components/Map/Base",tags:["autodocs"]},o={args:{baseMapProps:{lat:54.97,lng:-1.65,zoom:10,isDrawingMode:!1,style:{height:"500px",width:"500px"}}}};var t,a,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    baseMapProps: {
+      lat: 54.97,
+      lng: -1.65,
+      zoom: 10,
+      isDrawingMode: false,
+      style: {
+        height: "500px",
+        width: "500px"
+      }
+    }
+  }
+}`,...(e=(a=o.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const f=["Default"];export{o as Default,f as __namedExportsOrder,M as default};
+//# sourceMappingURL=Map.stories-7b44dc86.js.map
