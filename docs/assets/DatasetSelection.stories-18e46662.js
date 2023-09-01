8fd46be2ca791c9cca1b747ac77f275e9969a22b
amjones-scottlogic <97245023+amjones-scottlogic@users.noreply.github.com>
2023-09-01 16:00:25 +0000

    Deploying to gh-pages from  @ 63733b7354870ba88e81682e7f49768804cae5be 🚀

---
+++ docs/assets/DatasetSelection.stories-18e46662.js
@@ -0,0 +1,12 @@
+import{Q as p,M as i,a as l}from"./MapComponent-1e05941e.js";import{o as t}from"./jsxRuntime.module-8e7d8b33.js";import"./compat.module-ddc61323.js";import"./preact.module-4f38c496.js";import"./MapContainer-db993d88.js";import"./_commonjsHelpers-de833af9.js";const m=new p,c=({baseMapProps:r,showDatasets:n})=>t(l,{client:m,children:t(i,{baseMapProps:r,showDatasets:n})}),w={component:c,title:"Components/Map/Dataset Selection",tags:["autodocs"]},e={args:{baseMapProps:{lat:54.98,lng:-1.58,zoom:12.9,isDrawingMode:!1},showDatasets:!0}};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    baseMapProps: {
+      lat: 54.98,
+      lng: -1.58,
+      zoom: 12.9,
+      isDrawingMode: false
+    },
+    showDatasets: true
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const C=["Default"];export{e as Default,C as __namedExportsOrder,w as default};
+//# sourceMappingURL=DatasetSelection.stories-18e46662.js.map
---
+++ docs/assets/preview-914f9037.js
@@ -0,0 +1,2 @@
+const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
+//# sourceMappingURL=preview-914f9037.js.map
