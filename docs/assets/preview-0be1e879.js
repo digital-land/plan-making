a50759fd7baff4c51ea2d56432ebe7e21700531d
samornsby <samornsby@gmail.com>
2023-09-01 09:51:54 +0000

    Deploying to gh-pages from  @ 3e90f6aafc6bdabab144462fbbd9d14a333bb491 🚀

---
+++ docs/assets/preview-0be1e879.js
@@ -0,0 +1,2 @@
+const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
+//# sourceMappingURL=preview-0be1e879.js.map
