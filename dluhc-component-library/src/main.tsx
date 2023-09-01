5767b33247618837d63b1fc674c48d8156c0a932
Sam Ornsby <samornsby@gmail.com>
2023-09-01 10:39:39 +0100

    DLUHC-264 fix the casing for MapContainer in main

--- dluhc-component-library/src/main.tsx
+++ dluhc-component-library/src/main.tsx
@@ -1,7 +1,7 @@
 import { render } from "preact";
 import BaseMap from "src/components/maps/BaseMap.tsx";
 import DrawingLayer from "src/components/maps/DrawingLayer.tsx";
-import MapContainer from "src/components/maps/mapContainer.tsx";
+import MapContainer from "src/components/maps/MapContainer.tsx";
 import SiteSelectionForm from "src/components/siteSelectionForm/SiteSelectionForm.tsx";
 import TimetablePage from "src/components/timetablePage/TimetablePage.tsx";
 
