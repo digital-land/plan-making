ccab35cc87abbc66bdee9ce38d28b8febc5d9d6a
samornsby <samornsby@gmail.com>
2023-09-04 12:23:33 +0000

    Deploying to gh-pages from  @ 074d4839d9cf7f277811c19474fc1e714ed48f69 🚀

---
+++ docs/assets/DrawingLayer.stories-103c1f69.js
@@ -0,0 +1,22 @@
+import{h as c}from"./compat.module-ddc61323.js";import{Q as p,a as d,M as m}from"./MapComponent-76ba5b1a.js";import{o}from"./jsxRuntime.module-8e7d8b33.js";import{k as u}from"./preact.module-4f38c496.js";import"./MapContainer-478c92bd.js";import"./_commonjsHelpers-de833af9.js";const f=new p,g=({baseMapProps:a,drawingMapProps:s})=>{const[i,l]=c();return o(u,{children:o(d,{client:f,children:o(m,{baseMapProps:a,drawingMapProps:s,showDatasets:!1,value:i,onChange:l})})})},x={component:g,title:"Components/Map/Drawing Layer",tags:["autodocs"]},r={args:{baseMapProps:{lat:54.97,lng:-1.65,zoom:10,isDrawingMode:!0,style:{height:"500px",width:"500px"}},drawingMapProps:{strokeColor:"#ffcc33",fillColor:"rgba(255, 255, 255, 0.2)",strokeWidth:2,circleRadius:7,circleFillColor:"#ffcc33"}}};var e,n,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    baseMapProps: {
+      lat: 54.97,
+      lng: -1.65,
+      zoom: 10,
+      isDrawingMode: true,
+      style: {
+        height: "500px",
+        width: "500px"
+      }
+    },
+    drawingMapProps: {
+      strokeColor: "#ffcc33",
+      fillColor: "rgba(255, 255, 255, 0.2)",
+      strokeWidth: 2,
+      circleRadius: 7,
+      circleFillColor: "#ffcc33"
+    }
+  }
+}`,...(t=(n=r.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};const b=["Default"];export{r as Default,b as __namedExportsOrder,x as default};
+//# sourceMappingURL=DrawingLayer.stories-103c1f69.js.map
