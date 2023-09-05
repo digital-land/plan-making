1f44d294584834efd7e13e209fb00b819dbafd19
Beth Pritchard <bpritchard@scottlogic.com>
2023-09-05 11:29:59 +0100

    Create bucket and add policy in setup script

--- scripts/setupminio.sh
+++ scripts/setupminio.sh
@@ -1,6 +1,12 @@
 curl -o ~/minio.exe https://dl.min.io/server/minio/release/windows-amd64/minio.exe
 curl -o ~/mc.exe https://dl.min.io/client/mc/release/windows-amd64/mc.exe 
+echo "Minio downloaded" 
 
-mkdir ~/minio
+~/mc.exe alias set local http://127.0.0.1:9000 minioadmin minioadmin
 
-echo "Minio setup" 
+mkdir -p ~/minio
+
+~/mc.exe mb local/dluhc-poc
+~/mc.exe admin user add local testaccesskey testsecretkey
+~/mc.exe admin policy attach local readwrite --user testaccesskey
+
