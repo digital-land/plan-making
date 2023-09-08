d962b6ffe58cc50efe0fa0d022058a5072ffa1e1
amjones-scottlogic <97245023+amjones-scottlogic@users.noreply.github.com>
2023-09-08 12:46:44 +0000

    Deploying to gh-pages from  @ 9f029dbce3779627e68e74b86face0efbfeebab4 🚀

---
+++ docs/assets/DatasetSelection.stories-db5f5e65.js
@@ -0,0 +1,12 @@
+import{M as p,Q as i}from"./MapComponent-fa6d55dc.js";import{o as e}from"./jsxRuntime.module-8e7d8b33.js";import{Q as m}from"./queryClient-b1087e12.js";import"./compat.module-b1a3416f.js";import"./preact.module-4f38c496.js";import"./hooks.module-10a4f742.js";import"./MapContainer-2e634ccc.js";import"./_commonjsHelpers-de833af9.js";const l=new m,c=({baseMapProps:s,showDatasets:n})=>e(i,{client:l,children:e(p,{baseMapProps:s,showDatasets:n})}),Q={component:c,title:"Components/Map/Dataset Selection",tags:["autodocs"]},t={args:{baseMapProps:{lat:54.98,lng:-1.58,zoom:12.9,isDrawingMode:!1},showDatasets:!0}};var o,r,a;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    baseMapProps: {
+      lat: 54.98,
+      lng: -1.58,
+      zoom: 12.9,
+      isDrawingMode: false
+    },
+    showDatasets: true
+  }
+}`,...(a=(r=t.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const h=["Default"];export{t as Default,h as __namedExportsOrder,Q as default};
+//# sourceMappingURL=DatasetSelection.stories-db5f5e65.js.map
