b6d2efb73e9faef2b7a9245c810d833032e6193f
Sam Ornsby <samornsby@gmail.com>
2023-09-11 15:41:16 +0100

    DLUHC-201 add default date

--- dluhc-component-library/src/components/timetableForm/constants.ts
+++ dluhc-component-library/src/components/timetableForm/constants.ts
@@ -1,5 +1,6 @@
 import { NOT_STARTED } from "src/models/timetable/constants";
 import { FormState, Stage } from "./types";
+import { DateValue } from "../formComponents/dateInput/types";
 
 export const TITLE_KEY = "title";
 export const DESCRIPTION_KEY = "description";
@@ -7,9 +8,11 @@ export const PUBLISH_DATE_KEY = "publishDate";
 export const SCOPING_KEY = "scoping";
 export const GATEWAY_1_KEY = "gateway1";
 
+const DEFAULT_DATE: DateValue = { day: "", month: "", year: "" };
+
 const INITIAL_STAGE: Stage = {
-  startDate: { day: "", month: "", year: "" },
-  endDate: { day: "", month: "", year: "" },
+  startDate: DEFAULT_DATE,
+  endDate: DEFAULT_DATE,
   progress: NOT_STARTED,
   additionalInformation: "",
 };
@@ -17,7 +20,7 @@ const INITIAL_STAGE: Stage = {
 export const INITIAL_STATE: FormState = {
   [TITLE_KEY]: "",
   [DESCRIPTION_KEY]: "",
-  [PUBLISH_DATE_KEY]: { day: "", month: "", year: "" },
+  [PUBLISH_DATE_KEY]: DEFAULT_DATE,
   [SCOPING_KEY]: INITIAL_STAGE,
   [GATEWAY_1_KEY]: INITIAL_STAGE,
 };
