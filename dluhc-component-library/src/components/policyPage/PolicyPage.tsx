b6de32812974055cf57c81936e5d91c5d203805d
gcooperSL <110818566+gcooperSL@users.noreply.github.com>
2023-09-14 14:16:09 +0100

    changed policy page to use data from json

--- dluhc-component-library/src/components/policyPage/PolicyPage.tsx
+++ dluhc-component-library/src/components/policyPage/PolicyPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { PolicyData } from "../policyForm/types";
-import { loadCSV, loadJson } from "src/utils";
+import { loadJson } from "src/utils";
 interface PolicyPageProps {
   policyFilePath: string;
 }
@@ -34,42 +34,22 @@ const PolicyPage = ({ policyFilePath }: PolicyPageProps) => {
     <div>
       <div>
         <h1 className="my-8 text-3xl font-bold">
-          {policyData.reference}
-          {policyData.description}
-          BR1234 - Birmingham Local Plan Policy
+          {policyData.reference} - {policyData.title}
         </h1>
-        <p className="w-2/3 text-lg mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <p className="w-2/3 text-lg mb-8">{policyData.description}</p>
         <hr className="my-8"></hr>
         <h2 className="mt-8 text-xl font-bold">
           Themes this policy is linked to:
         </h2>
-        <p className="text-sm">Themes placeholder</p>
-        <p className="text-sm">Themes placeholder</p>
-        <p className="text-sm">Themes placeholder</p>
-        <p className="text-sm">Themes placeholder</p>
-
+        {policyData.themes.map((theme) => (
+          <p className="text-sm">{theme}</p>
+        ))}
         <h2 className="mt-8 text-xl font-bold">Requirements for policy:</h2>
-        <p className="text-sm">Requirements placeholder</p>
-        <p className="text-sm">Requirements placeholder</p>
-        <p className="text-sm">Requirements placeholder</p>
-        <p className="text-sm">Requirements placeholder</p>
-
+        {policyData.requirements.map((requirements) => (
+          <p className="text-sm">{requirements}</p>
+        ))}
         <p className="w-2/3 text-lg mb-8 mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          {policyData.supplementaryText}
         </p>
         <div>boundary placeholder</div>
       </div>
@@ -78,6 +58,3 @@ const PolicyPage = ({ policyFilePath }: PolicyPageProps) => {
 };
 
 export default PolicyPage;
-function csvToJson() {
-  throw new Error("Function not implemented.");
-}
