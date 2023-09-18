db53fa5f0584af84aa4cb2f274afb41f54fd5067
Louie Franchino <93914515+lfranchino@users.noreply.github.com>
2023-09-18 13:29:52 +0100

    Render multiple boundaries (#90)
    * built out site display functionality, stories need implementing
    
    * render preexisting boundaries
    
    * Removed unneccessary changes
    
    * specified min and max zoom
    
    * addressed comments
    
    * addressed comments

--- dluhc-component-library/src/components/maps/utils.ts
+++ dluhc-component-library/src/components/maps/utils.ts
@@ -1,6 +1,15 @@
 import { Geometry, Polygon } from "ol/geom";
+import Feature from "ol/Feature";
 import RenderFeature from "ol/render/Feature";
+
+import { Boundary } from "./types";
 
 export const isPolygon = (
   polygon: Geometry | RenderFeature | undefined,
 ): polygon is Polygon => polygon?.getType() === "Polygon";
+
+export const boundaryToFeature = (boundaries: Boundary[]) => {
+  return boundaries.map((boundary: Boundary) => {
+    return new Feature({ geometry: new Polygon(boundary as number[][][]) });
+  });
+};
