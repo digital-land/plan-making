479e935918981b75e14c17426884b19a53e9033d
gcooperSL <110818566+gcooperSL@users.noreply.github.com>
2023-09-29 09:48:43 +0100

    figma design default

--- dluhc-component-library/src/stories/SiteSelection.stories.tsx
+++ dluhc-component-library/src/stories/SiteSelection.stories.tsx
@@ -5,89 +5,6 @@ export default {
   title: "SOW13/Site Selection Form",
   tags: ["autodocs"],
 };
-
-export const Default = {
-  args: {
-    data: {
-      required: [
-        "siteUse",
-        "relationshipTo",
-        "address",
-        "name",
-        "age",
-        "consent",
-      ],
-      type: "object",
-      properties: {
-        name: {
-          type: "string",
-          title: "Your name",
-        },
-        consent: {
-          type: "boolean",
-          title: "I confirm consent for my data to be processed",
-        },
-        siteUse: {
-          type: "array",
-          items: { type: "string" },
-          title:
-            "What use or uses do you think should be considered for this site?",
-          subtitle: "You can choose as many uses as you'd like.",
-          enum: [
-            "Housing",
-            "Industry - including distribution or logistics uses",
-            "Commercial uses - office, retails, leisure, hotel or mixed",
-            "Community uses",
-            "Minerals - extracting, processing , and so on",
-            "Waste - recycling or processing",
-            "Tourism",
-            "Gypsy and Traveller site",
-            "Travelling Show-people site",
-            "New public green space",
-            "Renewable energy",
-            "Energy storage",
-            "Other",
-          ],
-        },
-        relationshipTo: {
-          type: "string",
-          title: "Your relationship to the site",
-          enum: [
-            "Landowner",
-            "Developer",
-            "Local Authority",
-            "Planning Agent",
-            "None",
-          ],
-        },
-        isBrownfieldSite: {
-          type: "boolean",
-          title: "Is this a Brownfield Site? ",
-        },
-        age: {
-          type: "number",
-          title: "Age in years",
-          step: 1,
-          min: 18,
-          max: 35,
-        },
-        address: {
-          type: "string",
-          title: "Please provide the fullest postal address you can",
-        },
-      },
-    },
-    uiSchema: {
-      relationshipTo: {
-        "ui:widget": "radio",
-      },
-      isBrownfieldSite: {
-        "ui:widget": "radio",
-      },
-    },
-  },
-};
-
 export const FigmaDesign = {
   args: {
     data: {
@@ -285,6 +202,88 @@ export const FigmaDesign = {
         "ui:widget": "textArea",
       },
       potentialSiteConstraints: {
+        "ui:widget": "radio",
+      },
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    data: {
+      required: [
+        "siteUse",
+        "relationshipTo",
+        "address",
+        "name",
+        "age",
+        "consent",
+      ],
+      type: "object",
+      properties: {
+        name: {
+          type: "string",
+          title: "Your name",
+        },
+        consent: {
+          type: "boolean",
+          title: "I confirm consent for my data to be processed",
+        },
+        siteUse: {
+          type: "array",
+          items: { type: "string" },
+          title:
+            "What use or uses do you think should be considered for this site?",
+          subtitle: "You can choose as many uses as you'd like.",
+          enum: [
+            "Housing",
+            "Industry - including distribution or logistics uses",
+            "Commercial uses - office, retails, leisure, hotel or mixed",
+            "Community uses",
+            "Minerals - extracting, processing , and so on",
+            "Waste - recycling or processing",
+            "Tourism",
+            "Gypsy and Traveller site",
+            "Travelling Show-people site",
+            "New public green space",
+            "Renewable energy",
+            "Energy storage",
+            "Other",
+          ],
+        },
+        relationshipTo: {
+          type: "string",
+          title: "Your relationship to the site",
+          enum: [
+            "Landowner",
+            "Developer",
+            "Local Authority",
+            "Planning Agent",
+            "None",
+          ],
+        },
+        isBrownfieldSite: {
+          type: "boolean",
+          title: "Is this a Brownfield Site? ",
+        },
+        age: {
+          type: "number",
+          title: "Age in years",
+          step: 1,
+          min: 18,
+          max: 35,
+        },
+        address: {
+          type: "string",
+          title: "Please provide the fullest postal address you can",
+        },
+      },
+    },
+    uiSchema: {
+      relationshipTo: {
+        "ui:widget": "radio",
+      },
+      isBrownfieldSite: {
         "ui:widget": "radio",
       },
     },
